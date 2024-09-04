ff19f4df79213f4581a08b5acb9b5c07796368a3
cemzzz <koreaok01@gmail.com>
2024-09-04 14:16:53 +0900

    탭 별 선택
    탭 선택에 따른 리스트가 다르게 나오도록 설정
    
    디자인 수정

--- main.js
+++ main.js
@@ -1,25 +1,41 @@
 let taskInput = document.getElementById('task-input'); // 유저 할 일 입력
 let taskTag = document.getElementById('task-tag'); // 유저 태그 입력
 let taskAddButton = document.getElementById('task-add-button'); // 할 일 추가 버튼
+let taskTagButton = document.getElementById('task-tag-button'); // 태그 추가 버튼
 let tagContainer = document.getElementById('tag-container'); // 태그 컨테이너
+
+let taskTabs = document.querySelectorAll(".task-tabs div") // 카테고리 분류
 let taskList = [] // 추가된 할 일 목록 저장
+let chosenTab = 'task-All'; // 선택한 탭 기본값은 task-All;
+let inProgressList = [] 
+let taskDoneList = [] 
 
 
 taskAddButton.addEventListener('click', taskAdd); // 할 일 추가 실행 이벤트
+taskTagButton.addEventListener('click', addTag); // 태그 추가 실행 이벤트
+
 taskTag.addEventListener('keydown', function(event) {
     if (event.key === 'Enter') {
         addTag();
     }
 });
+
 taskInput.addEventListener('keydown', function(event) {
     if (event.key === 'Enter') {
         taskAdd();
     }
 });
 
+//카테고리 클릭이벤트
+for(let i=0; i<taskTabs.length; i++){
+    taskTabs[i].addEventListener("click", function(event){
+        kategorie(event)
+    });
+}
+
 function addTag() {
-    let tagValue = taskTag.value.trim();
-    if (tagValue === '' || tagValue.startsWith('#')) {
+    let tagValue = taskTag.value.trim(); // 공백 제거
+    if (tagValue === '' || tagValue.startsWith('!')) {
         return; // 빈 값이나 '#'로 시작하는 값은 무시
     }
 
@@ -72,23 +88,32 @@ function taskDelete(id) {
 }
 
 function taskRender() {
-    let resultHTML = '';
-    for (let i = 0; i < taskList.length; i++) {
-        let tagsHTML = taskList[i].taskTags.map(tag => `<div class="task-content-tag">#${tag}</div>`).join('');
+    let chosenTabList = []
+    if (chosenTab === 'task-All') {
+        chosenTabList = taskList;
+    } else if (chosenTab === 'in-Progress') {
+        chosenTabList = inProgressList;
+    } else if (chosenTab === 'task-Done') {
+        chosenTabList = taskDoneList;
+    }
 
-        if (taskList[i].isComplete == true) {
+    let resultHTML = '';
+    for (let i = 0; i < chosenTabList.length; i++) {
+        let tagsHTML = chosenTabList[i].taskTags.map(tag => `<div class="task-content-tag">#${tag}</div>`).join('');
+
+        if (chosenTabList[i].isComplete == true) {
             resultHTML += 
             `<div class="task-complete-bg">
                 <div class='task-content-area'>
-                    <div class="task-complete">${taskList[i].taskContent}</div>
+                    <div class="task-complete">${chosenTabList[i].taskContent}</div>
                     <div class="task-content-tags">${tagsHTML}</div>
                 </div>
                 <div>
-                    <button onClick="toggleComplete('${taskList[i].id}')" class="task-prev-button">
+                    <button onClick="toggleComplete('${chosenTabList[i].id}')" class="task-prev-button">
                         <i class="fa-solid fa-rotate-left"></i>
                     </button>
-                    <button onClick="taskDelete('${taskList[i].id}')" class="task-delete-button">
-                        <i class="fa-solid fa-trash"></i>
+                    <button onClick="taskDelete('${chosenTabList[i].id}')" class="task-delete-button">
+                        <i class="fa-solid fa-x"></i>
                     </button>
                 </div>
             </div>`;
@@ -96,15 +121,15 @@ function taskRender() {
             resultHTML += 
             `<div class="task">
                 <div class='task-content-area'>
-                    <div class="task-content">${taskList[i].taskContent}</div>
+                    <div class="task-content">${chosenTabList[i].taskContent}</div>
                     <div class="task-content-tags">${tagsHTML}</div>
                 </div>
                 <div>
-                    <button onClick="toggleComplete('${taskList[i].id}')" class="task-complete-button">
+                    <button onClick="toggleComplete('${chosenTabList[i].id}')" class="task-complete-button">
                         <i class="fa-solid fa-check"></i>
                     </button>
-                    <button onClick="taskDelete('${taskList[i].id}')" class="task-delete-button">
-                        <i class="fa-solid fa-trash"></i>
+                    <button onClick="taskDelete('${chosenTabList[i].id}')" class="task-delete-button">
+                        <i class="fa-solid fa-x"></i>
                     </button>
                 </div>
             </div>`;
@@ -121,6 +146,39 @@ function toggleComplete(id) {
         }
     }
     taskRender();
+}
+
+function kategorie(event){
+    chosenTab = event.target.id;
+
+    // 모든 탭에서 active 클래스를 제거
+    for (let i = 0; i < taskTabs.length; i++) {
+        taskTabs[i].classList.remove('active');
+    }
+
+    // 클릭된 탭에 active 클래스 추가
+    event.target.classList.add('active');
+
+    inProgressList = [] // 진행중인 할 일
+    taskDoneList = [] // 완료된 일
+
+    if(chosenTab === "task-All"){
+        taskRender();
+    } else if(chosenTab === "in-Progress"){
+        for(let i=0; i<taskList.length; i++){
+            if(taskList[i].isComplete === false){
+                inProgressList.push(taskList[i])
+            }
+        }
+        taskRender();
+    } else if(chosenTab === "task-Done"){
+        for(let i=0; i<taskList.length; i++){
+            if(taskList[i].isComplete === true){
+                taskDoneList.push(taskList[i])
+            }
+        }
+        taskRender();
+    }
 }
 
 function randomUID() {
